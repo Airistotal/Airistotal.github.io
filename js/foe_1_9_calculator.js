1050bf2381425130430f684ed1e34531cb598374
Airistotal <danieltos.bouchard@gmail.com>
2020-10-24 15:21:02 -0500

    Update webgl and foe calculator

--- js/foe_1_9_calculator.js
+++ js/foe_1_9_calculator.js
@@ -1,13 +1,25 @@
 $(document).ready( function() {
   $("#calucate_output").on("click", function() {
-    let leftover_fps = $("#total_fps").val() - (
-      ($("#first_place_fps").val() * 1.9) + 
-      ($("#second_place_fps").val() * 1.9) + 
-      ($("#third_place_fps").val() * 1.9) + 
-      ($("#fourth_place_fps").val() * 1.9) + 
-      ($("#fifth_place_fps").val() * 1.9)
-    );
+    let first_place_fps = getAndProcessFps("first");
+    let second_place_fps = getAndProcessFps("second");
+    let third_place_fps = getAndProcessFps("third");
+    let fourth_place_fps = getAndProcessFps("fourth");
+    let fifth_place_fps = getAndProcessFps("fifth");
+
+    let leftover_fps = $("#total_fps").val() - (first_place_fps + second_place_fps + third_place_fps + fourth_place_fps + fifth_place_fps);
 
     $("#output").html(leftover_fps.toString());
   });
 });
+
+function getAndProcessFps(place) {
+    let place_fps = Math.ceil($(`#${place}_place_fps`).val() * 1.9);
+
+    if (place_fps === 0) {
+      place_fps = 1;
+    }
+
+    $(`#${place}_place_fps_result`).html(`= ${place_fps}`);
+
+    return place_fps;
+}
